beff32a9a3157ff5587c9563698a72b949a59b99
gauravghati <gauravghati225@gmail.com>
2022-09-11 12:37:22 +0530

    one more mockito testcase

--- e-commerce/src/test/java/com/springboot/ecommerce/ECommerceApplicationTests.java
+++ e-commerce/src/test/java/com/springboot/ecommerce/ECommerceApplicationTests.java
@@ -3,6 +3,7 @@ package com.springboot.ecommerce;
 import com.springboot.ecommerce.model.Employee;
 import com.springboot.ecommerce.repository.EmployeeRepository;
 import com.springboot.ecommerce.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -22,10 +24,11 @@ class ECommerceApplicationTests {
 	EmployeeServiceImpl employeeService;
 
 	@Test
+	@Order(1)
 	public void testGetAllEmployee() {
 		List<Employee> fewEmployees = new ArrayList<Employee>();
-		fewEmployees.add(new Employee("Gaurav", "Ghati", "gauravghatii@gmail.com"));
-		fewEmployees.add(new Employee("Ellika", "Mishra", "ellikamishra@gmail.com"));
+		fewEmployees.add(new Employee(1, "Gaurav", "Ghati", "gauravghatii@gmail.com"));
+		fewEmployees.add(new Employee(2, "Ellika", "Mishra", "ellikamishra@gmail.com"));
 
 		when(employeeRepository.findAll()).thenReturn(fewEmployees);
 
@@ -33,5 +36,26 @@ class ECommerceApplicationTests {
 		int sizeEmployeeList = employeeList.size();
 		assertEquals(2, sizeEmployeeList);
 		assertEquals(fewEmployees, employeeList);
+	}
+
+	@Test
+	@Order(2)
+	public void testGetEmployee() {
+		List<Employee> fewEmployees = new ArrayList<Employee>();
+		fewEmployees.add(new Employee(1, "Gaurav", "Ghati", "gauravghatii@gmail.com"));
+		fewEmployees.add(new Employee(2, "Ellika", "Mishra", "ellikamishra@gmail.com"));
+
+		long employeeId = 1;
+
+		Employee searchedEmp = new Employee(1, "test", "test", "test");
+		for( Employee emp : fewEmployees ){
+			if( emp.getId() == employeeId )
+				searchedEmp = emp;
+		}
+
+		when(employeeRepository.findById(employeeId)).thenReturn(Optional.of(searchedEmp));
+		Employee employee = employeeService.getEmployee(employeeId);
+
+		assertEquals(employee, searchedEmp);
 	}
 }
